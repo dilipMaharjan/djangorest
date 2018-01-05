cc682850a57e1a9d45995405e0f428b31f9d595b
dilipMaharjan <dilipmaharjann@gmail.com>
2018-01-05 10:49:21 -0500

    model serializer is cool

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail)
 ]
+# to be able to view format based response
+# e.g /snippets.json for json and /snippets.api for browsable api
+urlpatterns = format_suffix_patterns(urlpatterns)
