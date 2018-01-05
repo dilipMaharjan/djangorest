68edf98b16a547ae7b54da35c9bb386ac22699fe
dilipMaharjan <dilipmaharjann@gmail.com>
2018-01-05 11:18:08 -0500

    cbv

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail)
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view())
 ]
 # to be able to view format based response
 # e.g /snippets.json for json and /snippets.api for browsable api
