5df9b6b6d4dddfbf4a3743ca3415a5bf8a737a59
dilipMaharjan <dilipmaharjann@gmail.com>
2018-01-05 14:44:56 -0500

    routers

--- djangorest/urls.py
+++ djangorest/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='API')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('snippets.urls')),
     url(r'^api-auth/', include('rest_framework.urls')),
+    url(r'^schema/$', schema_view),
 ]
